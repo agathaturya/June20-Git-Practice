d86cc833943e98d3b16923afa854663f27d04924
agathaturyahikayo14@gmail.com <aturyahi@ndc.nasa.gov>
2017-06-20 10:26:02 -0400

    Added patient inflammation day 1 analysis script

---
+++ patient-analysis.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt(fname='data/inflammation-01.csv', delimiter=',')
+
+print(data)
+
+image-1=plt.plot(data)
+
+plt.show(image-1)
